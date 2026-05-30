6d4d861da1b7b36e85b1729e31cb241f80323644
MazMorrDev <marconchelo12@gmail.com>
2026-05-29 21:20:59 -0600

    feat: add ExpenseService and some methods

--- models/Expense.cs
+++ models/Expense.cs
@@ -1,7 +1,11 @@
-﻿namespace ExpenseTracker;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker;
 
 public class Expense
 {
+    [Key]
+    public int Id { get; set; }
     public string Description { get; set; } = string.Empty;
     public string Category { get; set; } = "General";
     public decimal Amount { get; set; }
---
+++ services/ExpenseService.cs
@@ -0,0 +1,25 @@
+﻿namespace ExpenseTracker;
+
+public class ExpenseService
+{
+    public Expense CreateExpense(string description, string category, decimal amount)
+    {
+        Expense expense = new()
+        {
+            Id = GetLastIdPlusOne(),
+            Amount = amount,
+            Description = description,
+            Category = category
+        };
+    }
+
+    public List<Expense> GetAllExpenses()
+    {
+        
+    }
+
+    public int GetLastIdPlusOne()
+    {
+
+    }
+}
