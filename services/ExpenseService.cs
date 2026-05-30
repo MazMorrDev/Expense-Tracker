d1565c6a28cb94c2ab31128907a37abc7c6bb680
MazMorrDev <marconchelo12@gmail.com>
2026-05-29 21:47:55 -0600

    feat(ExpenseService): add delete method

--- services/ExpenseService.cs
+++ services/ExpenseService.cs
@@ -1,7 +1,16 @@
-﻿namespace ExpenseTracker;
+﻿using TaskTrackerCLI.Services;
+
+namespace ExpenseTracker;
 
 public class ExpenseService
 {
+    private readonly StorageService _storageService;
+
+    public ExpenseService()
+    {
+        _storageService = new StorageService();
+    }
+
     public Expense CreateExpense(string description, string category, decimal amount)
     {
         Expense expense = new()
@@ -11,15 +20,22 @@ public class ExpenseService
             Description = description,
             Category = category
         };
+        List<Expense> expenses = GetAllExpenses();
+        expenses.Add(expense);
+        _storageService.Save(expenses);
+        return expense;
     }
 
     public List<Expense> GetAllExpenses()
     {
-        
+        return _storageService.Load();
     }
 
     public int GetLastIdPlusOne()
     {
-
+        List<Expense> expenses = GetAllExpenses();
+        // Knowed Bug: If a user deletes one intermedial expense
+        // then the IDs will be duplicated in the save operation
+        return expenses.Count + 1;
     }
 }
