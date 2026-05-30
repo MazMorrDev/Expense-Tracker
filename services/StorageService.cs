d15e4ee8e9b7dc880186c6fdec6ecfddafe25df1
MazMorrDev <marconchelo12@gmail.com>
2026-05-29 21:24:58 -0600

    feat: add StorageService with full implementations

---
+++ services/StorageService.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using ExpenseTracker;
+
+namespace TaskTrackerCLI.Services;
+
+public class StorageService
+{
+    private readonly string _dataFilePath;
+
+    public StorageService()
+    {
+        var projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", ".."));
+        var dataDir = Path.Combine(projectRoot, "Data");
+
+        if (!Directory.Exists(dataDir))
+            Directory.CreateDirectory(dataDir);
+
+        _dataFilePath = Path.Combine(dataDir, "data.json");
+    }
+
+    public List<Expense> Load()
+    {
+        if (!File.Exists(_dataFilePath))
+            return [];
+
+        string json = File.ReadAllText(_dataFilePath);
+
+        
+        if (string.IsNullOrWhiteSpace(json))
+            return [];
+
+        try
+        {
+            return JsonSerializer.Deserialize<List<Expense>>(json) ?? [];
+        }
+        catch (JsonException)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("El archivo data.json está dañado o no tiene formato válido. Se reiniciará. ദ്ദി(ᗜˬᗜ)");
+            Console.ResetColor();
+            return [];
+        }
+    }
+
+    public void Save(List<Expense>? expenses)
+    {
+        string json = JsonSerializer.Serialize(expenses, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(_dataFilePath, json);
+    }
+}
