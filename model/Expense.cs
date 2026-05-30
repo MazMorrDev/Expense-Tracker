de7b70b6a0052d01dd6b263d5a911a17e2bb6388
MazMorrDev <marconchelo12@gmail.com>
2026-05-29 21:07:42 -0600

    ref: remove some line spaces in Expense Model

--- model/Expense.cs
+++ model/Expense.cs
@@ -3,8 +3,6 @@
 public class Expense
 {
     public string Description { get; set; } = string.Empty;
-
     public string Category { get; set; } = "General";
-
     public decimal Amount { get; set; }
 }
